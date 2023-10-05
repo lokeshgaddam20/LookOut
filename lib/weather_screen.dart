0b4694e30e37f4b9e37cf8b9d71448f9b97bdbd1
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-10-05 23:37:24 +0530

    update keys.dart

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -5,7 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:weather_app/additional_info_item.dart';
 import 'package:weather_app/hourly_forecast_item.dart';
 import 'package:http/http.dart' as http;
-import 'package:weather_app/secrets.dart';
+import 'package:weather_app/keys.dart';
 
 class WeatherScreen extends StatefulWidget {
   final String cityName;
